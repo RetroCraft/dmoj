d4434bc07bf5718b707f9727936c2d53ce43d00b
James Ah Yong <retrocraft314@gmail.com>
2018-08-08 23:16:08 -0400

    vimscript + ccc04j1 + infinity + helloworld

---
+++ src/ccc04j1.java
@@ -0,0 +1,8 @@
+import java.io.*;
+import java.util.*;
+
+public class ccc04j1 {
+    public static void main(String[] args) {
+        System.out.println("The largest square has side length " + (int)Math.sqrt(new Scanner(System.in).nextInt()) + ".");
+    }
+}
