56e03ce5186896f7676a0ad40b6552a718fe415f
James Ah Yong <retrocraft314@gmail.com>
2018-08-08 23:47:47 -0400

    ccc14j1

---
+++ src/ccc14j1.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class ccc14j1 {
+  public static void main(String[] args) {
+    try {
+      BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+      int a = Integer.parseInt(in.readLine());
+      int b = Integer.parseInt(in.readLine());
+      int c = Integer.parseInt(in.readLine());
+      if (a + b + c != 180) {
+        System.out.println("Error");
+        return;
+      }
+      if (a == b && b == c) {
+        System.out.println("Equilateral");
+        return;
+      }
+      if (a == b || b == c || a == c) {
+        System.out.println("Isosceles");
+        return;
+      }
+      System.out.println("Scalene");
+    } catch (IOException e) { e.printStackTrace(); }
+  }
+}
